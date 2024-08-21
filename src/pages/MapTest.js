e30fcfdcdd23730a7ffcffb6afeaaf55c24bd402
Herman Lindalen Nodeland <afs0027473@agderskolen.no>
2024-08-22 00:06:01 +0200

    fixed map

--- src/pages/MapTest.js
+++ src/pages/MapTest.js
@@ -1,4 +1,4 @@
-import "../styling/Index.css";
+/*import "../styling/Index.css";
 
 import { Geocoder } from '@mapbox/search-js-react';
 
@@ -24,4 +24,39 @@ const Index = () => {
   );
 }
 
-export default Index;
+export default Index;*/
+
+
+
+import React, { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const Map = ({ accessToken }) => {
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
+
+    const map = new mapboxgl.Map({
+      container: 'map', // Container ID
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [7.86432, 58.1402624], // Starting position [lng, lat]
+      zoom: 10,
+    });
+
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+    });
+
+    map.addControl(geocoder);
+
+    return () => {
+      map.remove();
+    };
+  }, [accessToken]);
+
+  return <div id="map" style={{ width: '100%', height: '400px' }} />;
+};
+
+export default Map;
