4b59a031c314bcec752db7c05da9ed01b84b3ee3
Herman Lindalen Nodeland <afs0027473@agderskolen.no>
2024-08-22 16:13:15 +0200

    added slider library to the app

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,4 +1,10 @@
-import "../styling/Index.css";
+import "../styling/Index.css"; // Assuming this is your custom styling
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Modular style imports for Swiper 6+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 
 
 const Index = () => {
@@ -6,6 +12,11 @@ const Index = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Swiper spaceBetween={50} slidesPerView={1}>
+        <SwiperSlide>Page 1 Content</SwiperSlide>
+        <SwiperSlide>Page 2 Content</SwiperSlide>
+        {/* Add more SwiperSlide components for more pages */}
+      </Swiper>
 
       <button className="add-button"></button>
     </div>
