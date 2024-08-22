3e8fd5dc17d1a98bf7c40a5082a37cfd7f72a2ec
Herman Lindalen Nodeland <afs0027473@agderskolen.no>
2024-08-22 19:04:45 +0200

    added firebase integration to get data about ratings and restaurants

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,49 +1,73 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import "../styling/Index.css"; // Assuming this is your custom styling
+import "../styling/Index.css";
 import { Swiper, SwiperSlide } from 'swiper/react';
-// Modular style imports for Swiper 6+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+import { database } from '../utils/firebase';
+import { ref, onValue } from 'firebase/database';
 
 
 const Index = () => {
   const [activeSlide, setActiveSlide] = useState(0);
+
+
+
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const dbRef = ref(database, '/');
+      await onValue(dbRef, (snapshot) => {
+        const data = snapshot.val();
+        setData(data);
+      });
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
 
   return (
     <div className="swiper-container">
       <div className="overlaysContainer">
         {activeSlide === 0 ?
           <div className="pageOverlayContainer">
-            <b>Spise ute</b>
-            <p>Takeaway</p>
+            <b>Ratings</b>
+            <p>Restaurants</p>
           </div>
           :
           <div className="pageOverlayContainer">
-            <p>Spise ute</p>
-            <b>Takeaway</b>
+            <p>Ratings</p>
+            <b>Restaurants</b>
           </div>
         }
       </div>
 
       <Swiper spaceBetween={50} slidesPerView={1} onSlideChange={(swiper) => setActiveSlide(swiper.activeIndex)}>
         <SwiperSlide>
-          Page 1 Content
+          <div className="page1Content">
 
+
+
+            <button className="add-button"></button>
+          </div>
         </SwiperSlide>
 
 
         <SwiperSlide>
-          Page 2 Content
-          <button className="add-button"></button>
+          <div className="page2Content">
 
+
+
+          </div>
         </SwiperSlide>
       </Swiper>
 
       <div className="bottomNavbar">
-        
+
       </div>
     </div>
   )
